4e9907ff1ef20d12b3588a96a6f0adcff1f47975
Kelly de Vries <kdv@me.com>
2015-03-13 12:35:47 -0700

    Reset countRepeats function to if(in_array.... without foreach loop.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -7,20 +7,15 @@
         	$output = "";
         	$count = 0;
         	$string_array = explode(" ", $stringInput);
-
-        	foreach ($string_array as $string_word){
-	            if(in_array($wordInput, $string_array)){
-	            	$count = $count + 1;
-	            	$output = "match: ";
-	            }
-	            else {
-	            	$output = "not found";
-	            	$count = "";
-	            }
-
-			}
-
-
+        	
+            if(in_array($wordInput, $string_array)){
+            	$count = $count + 1;
+            	$output = "match: ";
+            }
+            else {
+            	$output = "not found";
+            	$count = "";
+            }
             return $output . $count;
         }
 
