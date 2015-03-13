df46dca0c045d4d5f8c56e12c9aaf1a91cafc2e7
Kelly de Vries <kdv@me.com>
2015-03-13 10:33:07 -0700

    Add tests of multiple letter words. Pass tests.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -17,6 +17,7 @@
             //Assert
             $this->assertEquals("match", $result);
         }
+
         function test_single_letter_no_match()
         {
             //Arrange
@@ -26,6 +27,34 @@
 
             //Act
             $result = $test_single_letter_no_match->countRepeats($wordInput, $stringInput);
+
+            //Assert
+            $this->assertEquals("not found", $result);
+        }
+                
+        function test_multi_letter_match()
+        {
+            //Arrange
+            $test_multi_letter_match = new RepeatCounter;
+            $wordInput = "am";
+            $stringInput = "am";
+
+            //Act
+            $result = $test_multi_letter_match->countRepeats($wordInput, $stringInput);
+
+            //Assert
+            $this->assertEquals("match", $result);
+        }
+
+        function test_multi_letter_no_match()
+        {
+            //Arrange
+            $test_multi_letter_no_match = new RepeatCounter;
+            $wordInput = "am";
+            $stringInput = "be";
+
+            //Act
+            $result = $test_multi_letter_no_match->countRepeats($wordInput, $stringInput);
 
             //Assert
             $this->assertEquals("not found", $result);
